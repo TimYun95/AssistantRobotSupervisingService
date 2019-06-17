6d36375060d4f0dd47f07cd6b4a085c81d3ab442
TimYun95 <timyun95@126.com>
2019-06-17 15:22:32 +0800

    Service created at 2019-06-17

---
+++ AssistantRobotSupervisingService/SupervisingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistantRobotSupervisingService
+{
+    public partial class SupervisingService : ServiceBase
+    {
+        public SupervisingService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+
+        }
+
+        protected override void OnStop()
+        {
+
+
+        }
+    }
+}
