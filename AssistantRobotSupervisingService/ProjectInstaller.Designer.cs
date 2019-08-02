f4b84b9bf50b3b2409e33bc48558354dea8bab75
TimYun95 <timyun95@126.com>
2019-08-02 15:54:34 +0800

    Correct service name

--- AssistantRobotSupervisingService/ProjectInstaller.Designer.cs
+++ AssistantRobotSupervisingService/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "Assistant Robot Supervising Ultra Service";
+            this.serviceInstaller1.ServiceName = "Assistant Robot Supervising Service";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
